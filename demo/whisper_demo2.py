1d09c4be14992778e504737726e74a2cd70c6a4a
samlee <ssli@microware1985.com>
2024-08-01 10:08:51 +0800

    add demo

--- demo/whisper_demo2.py
+++ demo/whisper_demo2.py
@@ -3,7 +3,7 @@ from xinference.client import Client
 client = Client("http://127.0.0.1:9997")
 
 model = client.get_model("whisper-base")
-with open("/Users/samlee/Documents/sample/asr/asr_example_zh.wav", "rb") as audio_file:
+with open("/Users/samlee/Documents/sample/asr/longwav_2.wav", "rb") as audio_file:
     print(model.transcriptions(
         audio=audio_file.read(),
         response_format='verbose_json'
